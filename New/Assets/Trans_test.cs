f68c4cb5c4cdb5c357a968dc5851c1b818252330
LIMMIHEE <s2018s19@e-mirim.hs.kr>
2020-01-18 18:37:52 +0900

    _

---
+++ New/Assets/Trans_test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trans_test : MonoBehaviour
+{
+    int A = 100;
+    string B = "1234";
+    float C = 1.3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(A.ToString());
+        Debug.Log(int.Parse(B));
+        Debug.Log((int)C);
+
+        Debug.Log(int.Parse("100") + 50);
+        Debug.Log(123456.ToString("N0"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
