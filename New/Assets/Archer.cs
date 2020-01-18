a8a1bc019ea752283dbda1e392d9c4ce5af67844
LIMMIHEE <s2018s19@e-mirim.hs.kr>
2020-01-18 18:20:36 +0900

    오랜만

---
+++ New/Assets/Archer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : MonoBehaviour
+{
+    public GameObject arrow;
+    public Transform shootXY;
+    public KeyCode shootKey;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(shootKey))
+        {
+            Instantiate(arrow).transform.position=shootXY.position;
+        }
+    }
+}
---
+++ New/Assets/auto_archer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class auto_archer : MonoBehaviour
+{
+    public GameObject arrowObj;
+    public Transform shootPos;
+    private int frame = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frame++;
+        if (frame %100==0)
+        {
+            Instantiate(arrowObj).transform.position = shootPos.position;
+        }
+    }
+}
