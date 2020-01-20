0c0bbf1e05864a511f102edd8b266fb3fd972224
LIMMIHEE <s2018s19@e-mirim.hs.kr>
2020-01-20 12:45:38 +0900

    어제까먹었다

---
+++ NewStudy_2/Assets/jump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour
+{
+    public GameObject testobj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector3.up * 300f);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            testobj.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            testobj.SetActive(true);
+        }
+    }
+}
---
+++ NewStudy_2/Assets/test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public GameObject arrobj;
+    public KeyCode shootKey;
+    public Transform shootPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(shootKey))
+        {
+            Instantiate(arrobj).transform.position = shootPos.position;
+        }
+    }
+}
