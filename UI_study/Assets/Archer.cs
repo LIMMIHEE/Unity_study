cee8e050a639ec0a8341d1d910add033b8c31a5f
LIMMIHEE <s2018s19@e-mirim.hs.kr>
2020-01-22 15:00:34 +0900

    020122

---
+++ UI_study/Assets/Archer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    public void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector3.up * 300f);
+    }
+}
---
+++ UI_study/Assets/scoretext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class scoretext : MonoBehaviour
+{
+    int score = 0;
+   
+    public void ScoreUp(int num)
+    {
+        score += num;
+        GetComponent<Text>().text = "Score :" + score;
+    }
+}
