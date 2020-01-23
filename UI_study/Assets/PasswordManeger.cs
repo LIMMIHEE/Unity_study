533a6f920bd0017628122628fb71ec13915ccf2d
LIMMIHEE <s2018s19@e-mirim.hs.kr>
2020-01-23 14:45:54 +0900

    020123

---
+++ UI_study/Assets/PasswordManeger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PasswordManeger : MonoBehaviour
+{
+    public InputField inputField;
+
+    public void Okbtn()
+    {
+        if (inputField.text.Equals("12345"))
+        {
+            Debug.Log("pass");
+        }
+        else
+        {
+            Debug.Log("false");
+        }
+
+    }
+}
