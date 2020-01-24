f345a75dba588ad4340f81ee20229f539fb26f62
LIMMIHEE <s2018s19@e-mirim.hs.kr>
2020-01-24 17:16:43 +0900

    020124

---
+++ Animation_study/Assets/Archer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : MonoBehaviour
+{
+    public void ShootBtn()
+    {
+        
+        GetComponent<Animator>().SetTrigger("shoot_t");
+        //GetComponent<Animator>().SetBool("TestBool",true);
+        float tempHp = GetComponent<Animator>().GetFloat("HP");
+        if (tempHp<10)
+        {
+            GetComponent<Animator>().SetTrigger("Die_t");
+        }
+        GetComponent<Animator>().SetFloat("HP", tempHp-10);
+    }
+}
