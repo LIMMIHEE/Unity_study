1dad34397f9936c0307e466297c656835cf1c3ec
LIMMIHEE <s2018s19@e-mirim.hs.kr>
2020-01-26 00:04:36 +0900

    020125
    모르고 까먹음

---
+++ Unity_Study_/Assets/saveText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class saveText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //    Debug.Log(PlayerPrefs.GetInt("HighScore", 100));
+        //PlayerPrefs.SetInt("HighScore", 1000);
+
+        Debug.Log(PlayerPrefs.GetString("PlayerName", "N"));
+        PlayerPrefs.SetString("PlayerName", "Marse");
+    }
+}
---
+++ Unity_Study_/Assets/scene_con.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene_con : MonoBehaviour
+{
+   public void ScenLoad(string scenName)
+    {
+        SceneManager.LoadScene(scenName);
+    }
+    public void ScenLoad(int scennum)
+    {
+        SceneManager.LoadScene(scennum);
+    }
+}
