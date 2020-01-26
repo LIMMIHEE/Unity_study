766d5f9a3683aa7ff96a4413ff6285e6e183838c
LIMMIHEE <s2018s19@e-mirim.hs.kr>
2020-01-27 03:08:41 +0900

    020127

---
+++ Run/Assets/char_m.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class char_m : MonoBehaviour
+{
+    public float moveSpeed = 0.05f;
+    void Update()
+    {
+        transform.Translate(Vector3.left * moveSpeed);
+    }
+}
