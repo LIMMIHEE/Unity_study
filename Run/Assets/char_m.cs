094333ca89f7cef1c68e9c21bf5b5292ea560af5
LIMMIHEE <s2018s19@e-mirim.hs.kr>
2020-02-03 16:36:22 +0900

    200203

--- Run/Assets/char_m.cs
+++ Run/Assets/char_m.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class char_m : MonoBehaviour
 {
+    public float JumpPower = 300f;
     public float moveSpeed = 0.05f;
+
+    public GameManager gameManager;
     void Update()
     {
         transform.Translate(Vector3.left * moveSpeed);
+    }
+
+    public void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector3.up * JumpPower);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.transform.name == "WinCollider")
+        {
+            gameManager.Win();
+        }
+        else if (col.transform.name == "loseCollider")
+        {
+            Debug.Log("Lose!");
+        }
     }
 }
